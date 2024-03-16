974cdb61ff68a395ca4b4151817390235d1332ba
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-03-15 20:35:03 -0500

    push

---
+++ utils/redis.js
@@ -0,0 +1,60 @@
+// utils/redis.js
+
+import redis from 'redis';
+
+class RedisClient {
+    constructor() {
+        this.client = redis.createClient();
+
+        this.client.on('error', (err) => {
+            console.error('Redis error:', err);
+        });
+    }
+
+    isAlive() {
+        return this.client.connected;
+    }
+
+    async get(key) {
+        return new Promise((resolve, reject) => {
+            this.client.get(key, (err, reply) => {
+                if (err) {
+                    console.error('Error getting value from Redis:', err);
+                    reject(err);
+                } else {
+                    resolve(reply);
+                }
+            });
+        });
+    }
+
+    async set(key, value, duration) {
+        return new Promise((resolve, reject) => {
+            this.client.set(key, value, 'EX', duration, (err, reply) => {
+                if (err) {
+                    console.error('Error setting value in Redis:', err);
+                    reject(err);
+                } else {
+                    resolve(reply);
+                }
+            });
+        });
+    }
+
+    async del(key) {
+        return new Promise((resolve, reject) => {
+            this.client.del(key, (err, reply) => {
+                if (err) {
+                    console.error('Error deleting value from Redis:', err);
+                    reject(err);
+                } else {
+                    resolve(reply);
+                }
+            });
+        });
+    }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
