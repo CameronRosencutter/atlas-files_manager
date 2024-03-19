4bf1fc366a010a17da042713159ada5c2f8df630
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-03-19 15:59:53 -0500

    appp

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,22 +1,33 @@
-// controllers/AppController.js
-
+// appcontroller
 const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
 const AppController = {
-    // Get status of Redis and MongoDB
-    getStatus: (request, response) => {
-      // Check if things are alive
-      const redisAlive = redisClient.isAlive();
-      const dbAlive = dbClient.isAlive();
-  
-      // Send status as JSON response
-      if (redisAlive && dbAlive) {
-        response.status(200).json({ redis: true, db: true });
-      } else {
-        response.status(500).json({ error: 'Service Not Available' });
-      }
-    },
-}
+
+  getStatus: (request, response) => {
+    // Check if things are alive
+    const redisAlive = redisClient.isAlive();
+    const dbAlive = dbClient.isAlive();
+
+
+    if (redisAlive && dbAlive) {
+      response.status(200).json({ redis: true, db: true });
+    } else {
+      response.status(500).json({ error: 'Service Not Available' });
+    }
+  },
+
+
+  getStats: async (request, response) => {
+    try {
+      const usersCount = await dbClient.nbUsers();
+      const filesCount = await dbClient.nbFiles();
+
+      response.status(200).json({ users: usersCount, files: filesCount });
+    } catch (error) {
+      response.status(500).json({ message: 'Error retrieving statistics' });
+    }
+  },
+};
 
 module.exports = AppController;
