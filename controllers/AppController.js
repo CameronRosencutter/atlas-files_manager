ce33c05b5e8c00cdd072593c0560c35263800281
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-03-19 15:52:45 -0500

    pussh appp

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,23 +1,22 @@
 // controllers/AppController.js
 
-const { checkRedis, checkDB } = require('../utils');
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
 
 const AppController = {
-  getStatus: (req, res) => {
-    const redisAlive = checkRedis();
-    const dbAlive = checkDB();
-    if (redisAlive && dbAlive) {
-      return res.status(200).json({ redis: true, db: true });
-    } else {
-      return res.status(500).json({ error: "Server error" });
-    }
-  },
-  getStats: (req, res) => {
-    // Logic to count users and files from respective collections
-    const usersCount = 12; // Logic to count users
-    const filesCount = 1231; // Logic to count files
-    return res.status(200).json({ users: usersCount, files: filesCount });
-  }
-};
+    // Get status of Redis and MongoDB
+    getStatus: (request, response) => {
+      // Check if things are alive
+      const redisAlive = redisClient.isAlive();
+      const dbAlive = dbClient.isAlive();
+  
+      // Send status as JSON response
+      if (redisAlive && dbAlive) {
+        response.status(200).json({ redis: true, db: true });
+      } else {
+        response.status(500).json({ error: 'Service Not Available' });
+      }
+    },
+}
 
 module.exports = AppController;
