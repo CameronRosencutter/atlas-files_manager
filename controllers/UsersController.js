3dd8fa6a3ddd3558ba309dc5be0383a145b4875e
kris_cross_applesauce <karisrr2203@gmail.com>
2024-03-19 17:25:05 -0500

    3: ok, I'm starting to get it better now, main changes were: return Error('message'), 400 -> response.status(400).json({ error: 'message' }); and changed whatever that crypto thing was to SHA1 (weird that the first results for hashing stuff in SHA1 is to do it with the crypto module instead of the SHA1 module)

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,32 +1,20 @@
+const SHA1 = require('sha1');
 const dbClient = require('../utils/db');
-const crypto = require('crypto');
 
 const UsersController = {
-  postNew: (email, password) => {
+  postNew: (request, response) => {
+    const { email, password } = request.body;
     const usersCollection = dbClient.client.db(dbClient.database).collection('users');
 
-    if (!email) {
-      const err = new Error('Missing email');
-      err.code = 400;
-      return err;
-    }
-    if (!password) {
-      const err = new Error('Missing password');
-      err.code = 400;
-      return err;
-    }
-    if (usersCollection.find({email: email})) {
-      const err = new Error('Already exist');
-      err.code = 400;
-      return err;
+    if (!email) { return response.status(400).json({ error: 'Missing email' }); }
+    if (!password) { return response.status(400).json({ error: 'Missing password' }); }
+    if (usersCollection.find({ email: email })) {
+      return response.status(400).json({ error: 'Already exist' });
     }
 
-    const user = {
-      email: email,
-      password: crypto.createHash('SHA1').update(password).digest('hex'),
-    };
-    return usersCollection.insertOne(user);
-  }
-}
+    const user = usersCollection.insertOne({ email: email, password: SHA1(password) });
+    return response.status(201).json({ id: user._id, email: email });
+  },
+};
 
 module.exports = UsersController;
