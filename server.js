b8af96afd1f586e205a3e9267e4cc5fb706f4a1c
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-03-19 14:41:41 -0500

    f

---
+++ server.js
@@ -0,0 +1,13 @@
+// server.js
+
+const express = require('express');
+const routes = require('./routes');
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
