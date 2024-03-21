02805f1c5e3194628ffe23cd8c55f29bb8bc22ec
kris_cross_applesauce <karisrr2203@gmail.com>
2024-03-20 22:16:21 -0500

    server.js: bodyParser.json() -> express.json()

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 // server.js
 
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const routes = require('./routes');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+// // parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }));
 
-// parse application/json
-app.use(bodyParser.json());
+// // parse application/json
+// app.use(bodyParser.json());
+app.use(express.json());
 
 app.use(routes);
 
