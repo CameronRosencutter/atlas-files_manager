77232369f36a08c1b9240134147a9a55e62a1830
kris_cross_applesauce <karisrr2203@gmail.com>
2024-03-20 23:34:42 -0500

    4: feeling pretty good about this

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,18 @@
 const express = require('express');
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
-// const FilesController = require('../controllers/FilesController');
+const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+
 router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 // router.post('/files', FilesController.postUpload);
 // // GET /files/:id => FilesController.getShow
