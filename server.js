cd5d53e9fe6598210d2baeb2ae5d8d4066bc8d40
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-06-10 17:19:55 -0500

    add server file

---
+++ server.js
@@ -0,0 +1,16 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+/* eslint-disable linebreak-style */
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use('/', routes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
