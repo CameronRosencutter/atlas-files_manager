2d409ca5f760b9d5185df7a8b7919d24503d6898
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-06-10 13:55:35 -0500

    Beginning Redis and redisclient

---
+++ utils/redis.js
@@ -0,0 +1,10 @@
+import redis from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    this.client.on('error', (err) => {
+      console.error('Redis client error:', err);
+    });
+  }
