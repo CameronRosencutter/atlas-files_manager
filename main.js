83af967f2d5d9a61a80755ebe8a64c3bb5ee473a
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-06-10 16:55:59 -0500

    finished db.js

--- main.js
+++ main.js
@@ -1,20 +1,41 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable prefer-promise-reject-errors */
+/* eslint-disable linebreak-style */
+/* eslint-disable arrow-body-style */
+/* eslint-disable linebreak-style */
 /* eslint-disable arrow-parens */
 /* eslint-disable indent */
 /* eslint-disable linebreak-style */
-import redisClient from './utils/redis';
+import dbClient from './utils/db';
+
+const waitConnection = () => {
+  return new Promise((resolve, reject) => {
+    let i = 0;
+    const repeatFct = async () => {
+      await setTimeout(() => {
+        i += 1;
+        if (i >= 10) {
+          reject('Connection to MongoDB failed');
+        } else if (!dbClient.isAlive()) {
+          repeatFct();
+        } else {
+          resolve();
+        }
+      }, 1000);
+    };
+    repeatFct();
+  });
+};
 
 (async () => {
-    // Adding a small delay to allow the Redis client to connect
-    await new Promise(resolve => setTimeout(resolve, 1000));
-
-    console.log('Redis client is alive:', redisClient.isAlive());
-
-    console.log('Initial value of myKey:', await redisClient.get('myKey'));
-    await redisClient.set('myKey', 12, 5);
-    console.log('Value of myKey after setting:', await redisClient.get('myKey'));
-
-    setTimeout(async () => {
-        console.log('Value of myKey after 10 seconds:', await redisClient.get('myKey'));
-    }, 10000);
+  console.log(`MongoDB is alive: ${dbClient.isAlive()}`);
+  await waitConnection();
+  console.log(`MongoDB is alive: ${dbClient.isAlive()}`);
+  
+  const usersCount = await dbClient.nbUsers();
+  console.log(`Number of users: ${usersCount}`);
+  
+  const filesCount = await dbClient.nbFiles();
+  console.log(`Number of files: ${filesCount}`);
 })();
