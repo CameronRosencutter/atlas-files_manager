8e99ff4264623ab9ce149a7550a5ee3204ccfed8
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-06-10 14:02:31 -0500

    editing redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -1,3 +1,7 @@
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line linebreak-style
+/* eslint-disable consistent-return */
+/* eslint-disable linebreak-style */
 import redis from 'redis';
 
 class RedisClient {
@@ -8,3 +12,38 @@ class RedisClient {
       console.error('Redis client error:', err);
     });
   }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, value) => {
+        if (err) return reject(err);
+        resolve(value);
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (err) => {
+        if (err) return reject(err);
+        resolve();
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err) => {
+        if (err) return reject(err);
+        resolve();
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
