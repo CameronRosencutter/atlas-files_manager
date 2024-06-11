513ab107f644aa18ea8c0b4fb7dd3536718fb633
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-06-11 11:55:29 -0500

    pushing files again

---
+++ controllers/AppController.js
@@ -0,0 +1,21 @@
+/* eslint-disable linebreak-style */
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class AppController {
+  static async getStatus(req, res) {
+    const redisAlive = redisClient.isAlive();
+    const dbAlive = dbClient.isAlive();
+
+    res.status(200).json({ redis: redisAlive, db: dbAlive });
+  }
+
+  static async getStats(req, res) {
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
+
+    res.status(200).json({ users: usersCount, files: filesCount });
+  }
+}
+
+export default AppController;
---
+++ routes/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable import/extensions */
+/* eslint-disable linebreak-style */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line import/extensions
+import express from 'express';
+import AppController from '../controllers/AppController';
+
+const router = express.Router();
+
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+
+export default router;
