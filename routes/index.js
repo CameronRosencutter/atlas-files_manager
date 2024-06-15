238c747b7c8d8bb052b28946f1186e8fa1a76302
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-06-14 19:19:45 -0500

    adding UsersControllers

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,12 @@
 // eslint-disable-next-line import/extensions
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
 
 export default router;
