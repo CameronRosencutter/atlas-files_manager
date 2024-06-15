2469b00539d52e3c62cbb15efedf79c731a9a0d6
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-06-14 19:50:15 -0500

    update

--- utils/db.js
+++ utils/db.js
@@ -6,15 +6,21 @@ class DBClient {
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
-
     const url = `mongodb://${host}:${port}`;
-    this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-    this.client.connect().catch((err) => console.error('MongoDB connection error:', err));
-    this.db = this.client.db(database);
+
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.client.connect()
+      .then(() => {
+        this.db = this.client.db(database);
+        console.log('Connected to MongoDB');
+      })
+      .catch((err) => {
+        console.error('MongoDB connection error:', err);
+      });
   }
 
   isAlive() {
-    return this.client.topology.isConnected();
+    return this.client.isConnected();
   }
 
   async nbUsers() {
