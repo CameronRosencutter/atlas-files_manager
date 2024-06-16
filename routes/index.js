44e872f5e74cadd66ed197a7f78bab7264c13110
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-06-15 23:31:27 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,25 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
+/* eslint-disable no-multi-spaces */
+/* eslint-disable linebreak-style */
 /* eslint-disable import/extensions */
 /* eslint-disable linebreak-style */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable linebreak-style */
 // eslint-disable-next-line import/extensions
+
 import express from 'express';
 import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';  // Import AuthController
+import UsersController from '../controllers/UsersController'; // Import UsersController
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);  // Add connect endpoint
+router.get('/disconnect', AuthController.getDisconnect);  // Add disconnect endpoint
+router.get('/users/me', UsersController.getMe);  // Add getMe endpoint
 
 export default router;
